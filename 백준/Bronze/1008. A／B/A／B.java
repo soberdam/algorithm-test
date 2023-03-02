c2bf548991c93f3f970e0947e2e8255fdf7e575f
조담현 <j38903639@gmail.com>
2023-03-02 22:32:18 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        double a = (double)scanner.nextInt();
+        double b = (double)scanner.nextInt();
+        System.out.print(a/b);
+    }
+}
