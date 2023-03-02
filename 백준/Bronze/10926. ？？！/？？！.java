ad9ca9f49e60d126892b660b56ecb34bf25edddb
조담현 <j38903639@gmail.com>
2023-03-02 22:46:10 +0900

    [Bronze V] Title: ??!, Time: 200 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String id = scanner.next();
+        
+        System.out.print(id+"??!");
+    }
+}
