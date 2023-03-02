6e07e75867cf30b19f33690f81d7005ac128dafc
조담현 <j38903639@gmail.com>
2023-03-02 22:17:44 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Long a = scanner.nextLong();
+        Long b = scanner.nextLong();
+        Long c = scanner.nextLong();
+        System.out.print(a+b+c);
+    }
+}
