b6c447d69e7671c17d55969bfe9806279803cb6a
조담현 <j38903639@gmail.com>
2023-03-08 21:14:47 +0900

    [Bronze I] Title: 평균, Time: 260 ms, Memory: 18808 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        int subjectCount = scanner.nextInt();
+        double sumScore = 0.0;
+        double maxScore = 0.0;
+        double [] scoreArr = new double[subjectCount];
+        for(int i=0; i<subjectCount; i++){
+            scoreArr[i] = scanner.nextInt();
+        }
+    
+        
+        for(int i=0; i<subjectCount; i++){
+            if(scoreArr[i]>maxScore) {
+                maxScore = scoreArr[i];
+            }
+        }
+        
+        for(int i=0; i<subjectCount; i++){
+            scoreArr[i] = scoreArr[i]/maxScore*100;
+            sumScore += scoreArr[i];
+        }
+        
+        double modifiedAvg = sumScore/subjectCount;
+        System.out.print(modifiedAvg);
+        
+    }
+}
