354aaa1df9028868ab6e8c0bebfdc93e716f9a21
조담현 <j38903639@gmail.com>
2023-03-09 09:53:31 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        char inputChar = str.charAt(0);
+        int asciiCode = inputChar;
+        
+        System.out.print(asciiCode);
+        
+    }
+}
