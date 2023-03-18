82693a63b46bdbe76cc85541d7862bd3c7d44f5d
조담현 <j38903639@gmail.com>
2023-03-18 22:34:24 +0900

    [Bronze V] Title: 팩토리얼, Time: 204 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int factorial = 1;
+        for(int i=1; i<=num; i++){
+            factorial *= i;
+        }
+        System.out.println(factorial);
+    }
+}
