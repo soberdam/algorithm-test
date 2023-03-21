c31bd56c1bfc8645147e51df657c6565d427b039
조담현 <j38903639@gmail.com>
2023-03-21 18:35:18 +0900

    문제 풀이 추가

--- this-is-coding-test-for-job/Implementation/Time.java
+++ this-is-coding-test-for-job/Implementation/Time.java
@@ -5,6 +5,7 @@
 package Implementation;
 import java.util.*;
 public class Time {
+	// 특정한 시각에 안에 '3'이 포함되어 있는 지의 여부
 	public static boolean check(int h, int m, int s) {
 		if(h % 10 == 3 || m / 10 == 3 || m % 10 == 3 || s / 10 == 3 || s % 10 == 3) {
 			return true;
@@ -14,12 +15,14 @@ public class Time {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		// h를 입력받기
 		int h = scanner.nextInt();
 		int cnt = 0;
 		
 		for(int i=0; i<=h; i++) {
 			for(int j=0; j<60; j++) {
 				for(int k=0; k<60; k++) {
+					// 매 시각 안에 3이 포함되어 있다면 카운트 증가
 					if (check(i, j, k)) {
 						cnt++;
 					}
