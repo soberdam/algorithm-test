9219370029a81e2dd155d34633ad7d501e9f08ce
조담현 <j38903639@gmail.com>
2023-07-24 16:46:49 +0900

    [Silver IV] Title: 수 찾기, Time: 552 ms, Memory: 56804 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,39 +1,60 @@
-import java.io.*;
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine());
-		HashMap <Integer, Integer> map = new HashMap();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		int[] nNum = new int[N];
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<n; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			if(map.containsKey(num)) {
-				continue;
-			} else {
-				map.put(num, 1);
-			}
+		for(int i = 0; i < N; i++) {
+			nNum[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		Arrays.sort(nNum);
 		
-		int m = Integer.parseInt(br.readLine());
+				
+		int M = Integer.parseInt(br.readLine());
+				
+		int low, high, mid;
+		
 		st = new StringTokenizer(br.readLine());
 		
-		for(int j=0; j<m; j++) {
-			int num = Integer.parseInt(st.nextToken());
-			if(map.containsKey(num)) {
-				bw.write(1 + "\n");
-			} else {
-				bw.write(0 + "\n");
+		Loop1 : for(int i = 0, temp = 0; i < M; i++) {
+			temp = Integer.parseInt(st.nextToken());
+			
+			low = 0;
+			high = N-1;
+						
+			while(low <= high) {
+				mid = (low + high) / 2;
+				
+				if(nNum[mid] == temp) {
+					sb.append("1\n");
+					continue Loop1;
+				}
+				else if(nNum[mid] > temp) {
+					high = mid - 1;
+				}
+				else if(nNum[mid] < temp){
+					low = mid + 1;
+				}
 			}
+			
+			sb.append("0\n");
 		}
 		
+		bw.write(String.valueOf(sb));
 		bw.flush();
 		bw.close();
- 	}
-	
+	}
 
 }
-
