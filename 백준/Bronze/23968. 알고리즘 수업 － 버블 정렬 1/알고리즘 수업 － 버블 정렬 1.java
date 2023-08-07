64bf18e871f3fb040600de5a4a693b8446d9a1b9
조담현 <j38903639@gmail.com>
2023-08-07 15:38:08 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 1, Time: 376 ms, Memory: 17164 KB -BaekjoonHub

--- 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.java
+++ 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.java
@@ -1,49 +1,44 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		int[] arr = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		//출력 값을 위한 변수 선언
-		int small = 0;
-		int big = 0;
-		
-		//K번째 까지 카운트
-		int count = 0;
-		
-		
-		
-		Loop1 : for(int i = 1; i < N; i++) {
-			for(int j = 0; j < N - i; j++) {
-				if(arr[j] > arr[j+1]) {
-					big = arr[j];
-					small = arr[j+1];
-					arr[j+1] = big;
-					arr[j] = small;
-					count++;
-					if(count == K) break Loop1;
-				}
-			}
-		}
-
-		if(count < K) System.out.println(-1);
-		else System.out.println(small + " " + big);
+    public static void main(String[] args) throws IOException {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st = new StringTokenizer(br.readLine());
+	
+	int n = Integer.parseInt(st.nextToken());
+	int k = Integer.parseInt(st.nextToken());
+	int[] arr = new int[n];
+	st = new StringTokenizer(br.readLine());
+	for(int i=0; i<n; i++) {
+	    int num = Integer.parseInt(st.nextToken());
+	    arr[i] = num;
 	}
-
+	
+	bubbleSort(arr, k);
+	
+    }
+    
+    
+    public static void bubbleSort(int[]arr, int k) {
+	int count = 0;
+	for(int i=arr.length-1; i>0; i--) {
+	    for(int j=0; j<arr.length-1; j++) {
+		if(arr[j]>arr[j+1]) {
+		    count++;
+		    if(count == k) {
+			System.out.println(arr[j+1]+" "+arr[j]);
+			return;
+		    }
+		    int tmp = arr[j+1];
+		    arr[j+1] = arr[j];
+		    arr[j] = tmp;
+		}
+	    }
+	}
+	
+	System.out.println(-1);
+    }
 }
