b9a98767bdfdec295ed808aaa2c3df4ac9bd73f2
조담현 <j38903639@gmail.com>
2023-08-08 11:03:10 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 132 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,30 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	int n = Integer.parseInt(br.readLine());
+	System.out.println(fib(n));
+    }
+
+    public static int fib(int n) {
+	if(n == 0) {
+	    return 0;
+	}
+	
+	if(n == 1) {
+	    return 1;
+	}
+	
+	return fib(n-1) + fib(n-2);
+    }
+
+    
+
+}
