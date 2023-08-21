1a279e32eefedbf57ff8cd9c0fbddf9610d47833
조담현 <j38903639@gmail.com>
2023-08-21 14:33:46 +0900

    [Silver I] Title: 오목, Time: 152 ms, Memory: 16028 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.java
+++ 백준/Silver/2615. 오목/오목.java
@@ -17,12 +17,13 @@ public class Main {
 			}
 		}
 
-		// 동, 남, 남동, 남서
-		int[] dr = { 0, 1, 1, 1 };
-		int[] dc = { 1, 0, 1, -1 };
+		// 북, 동, 북동, 남동
+		int[] dr = { -1, 0, -1, 1 };
+		int[] dc = { 0, 1, 1, 1 };
 
-		for (int r = 0; r < 19; r++) {
-			for (int c = 0; c < 19; c++) {
+		// 세로로 우방향 탐색하면 항상 가장 왼쪽의 점부터 탐색할 수 있다.
+		for (int c = 0; c < 19; c++) {
+			for (int r = 0; r < 19; r++) {
 				// 0 혹은 1이라면 오목 탐색
 				if (board[r][c] != 0) {
 					// 4방향 탐색 시작
@@ -30,50 +31,37 @@ public class Main {
 						int nr = r;
 						int nc = c;
 						int count = 1;
-						int[][] stones = new int [5][2];
-						stones[0][0] = r;
-						stones[0][1] = c;
-						int idx = 1;
+
 						// 정방향 탐색 (동, 남, 남동, 남서)
 						while (true) {
 							nr += dr[d];
 							nc += dc[d];
 							if (nr >= 0 && nr < 19 && nc >= 0 && nc < 19 && board[nr][nc] == board[r][c]) {
 								count++;
-								if(count>5) break;
-								stones[idx][0] = nr;
-								stones[idx][1] = nc;
-								idx++;
 							} else {
 								break;
 							}
 						}
-						
+
 						nr = r;
 						nc = c;
-						
+
 						// 역방향 탐색 (서, 북, 북서, 북동)
 						while (true) {
 							nr -= dr[d];
 							nc -= dc[d];
 							if (nr >= 0 && nr < 19 && nc >= 0 && nc < 19 && board[nr][nc] == board[r][c]) {
 								count++;
-								if(count>5) break;
-								stones[idx][0] = nr;
-								stones[idx][1] = nc;
-								idx++;
+
 							} else {
 								break;
 							}
 						}
-						
+
 						if (count == 5) {
 							System.out.println(board[r][c]);
-							Arrays.sort(stones, ((a,b)-> {
-								return a[1] - b[1];
-							}));
-							
-							System.out.println((stones[0][0]+1) + " " + (stones[0][1]+1));
+
+							System.out.println((r + 1) + " " + (c + 1));
 							return;
 
 						}
