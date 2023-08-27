0be9bb1cc8a57e65cb8788b97912ef1b55542494
조담현 <j38903639@gmail.com>
2023-08-28 00:11:50 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 212 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int V = sc.nextInt();
+        int day= 0;
+        
+        day = (V - A + (A-B)-1) / (A-B) + 1;
+        
+        System.out.println(day);
+    }
+ 
+    
+}
