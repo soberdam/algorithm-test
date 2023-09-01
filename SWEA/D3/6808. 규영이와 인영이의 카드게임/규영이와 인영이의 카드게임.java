caf59494541934e48281e025b4a4c5106598c414
조담현 <j38903639@gmail.com>
2023-09-01 14:35:26 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 2,153 ms, Memory: 21,992 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -7,7 +7,6 @@ import java.util.StringTokenizer;
 class Solution {
 	static int[] selected; // 규영이, 순서 고정
 	static int[] unSelected; // 인영이가 가진 카드, 순서 9!가지 경우의 수
-	static ArrayList<Integer> list; // 순서인 9개를 저장할 곳
 	static int win;
 	static int lose;
 	public static void main(String args[]) throws Exception {
@@ -17,7 +16,6 @@ class Solution {
 		for (int tc = 1; tc <= T; tc++) {
 			selected = new int[9];
 			unSelected = new int[9];
-			list = new ArrayList<Integer>();
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < 9; i++) {
 				int num = Integer.parseInt(st.nextToken());
@@ -35,19 +33,13 @@ class Solution {
 			}
 			win = 0;
 			lose = 0;
-			DFS();
+			DFS(0, 0, 0);
 			System.out.printf("#%d %d %d\n", tc, win, lose);
 		}
 	}
 	
-	public static void DFS() {
-		if(list.size()==9) {
-			int sumA=0;
-			int sumB=0;
-			for(int i=0; i<9; i++) {
-				if(selected[i]>list.get(i)) sumA+= selected[i]+list.get(i);
-				else if(selected[i]<list.get(i))sumB += selected[i]+list.get(i);
-			}
+	public static void DFS(int sumA, int sumB, int count) {
+		if(count==9) {
 			if(sumA>sumB) win++;
 			else if(sumB>sumA) lose++;
 			return;
@@ -55,12 +47,23 @@ class Solution {
 		
 		for(int i=0; i<9; i++) {
 			if(unSelected[i]!=0) {
+				boolean isWin = false;
 				int temp = unSelected[i];
-				list.add(temp);
+				if(selected[count]>unSelected[i]) {
+					sumA+= selected[count] + unSelected[i];
+					isWin = true;
+				}
+				else if(selected[count]<unSelected[i]) sumB+= selected[count] + unSelected[i];
+				count++;
 				unSelected[i] = 0;
-				DFS();
-				list.remove(list.size()-1);
+				DFS(sumA, sumB, count);
+				count--;
 				unSelected[i] = temp;
+				if(isWin) {
+					sumA-= selected[count] + unSelected[i];
+				} else {
+					sumB-= selected[count] + unSelected[i];
+				}
 			}
 		}
 	}
