3588ba29a2e597b2e146cd867ad0442082a21c71
조담현 <j38903639@gmail.com>
2023-10-04 01:12:35 +0900

    [Silver IV] Title: 설탕 배달, Time: 208 ms, Memory: 17772 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -3,25 +3,44 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int input = scan.nextInt();
-		int count = 0;
 
-		while (true) {
-			if (input % 5 == 0) {
-				count += input / 5;
-				break;
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		boolean bool = false;
+		int answer = -1;
+
+		Loop1: for (int i = n / 5 * 5; i >= 5; i -= 5) {
+			// 1. 5로 나누어질때
+			if (n - i == 0) {
+				answer = i / 5;
+				bool = true;
+				break Loop1;
 			}
-			
-			if (input < 3) {
-				count = -1;
-				break;
+
+			// 2. 5의 배수로 나눈 나머지가 3의 배수으로 나누어질때
+			for (int j = n / 3 * 3; j >= 3; j -= 3) {
+
+				if ((n - i) - j == 0) {
+					answer = i / 5 + j / 3;
+					bool = true;
+					break Loop1;
+				}
+
 			}
-			input -= 3;
-			count++;
 
 		}
-		System.out.println(count);
+		// 3. 3으로 나누어질 때
+		if (!bool) {
+			for (int j = n / 3 * 3; j >= 3; j -= 3) {
+				if (n % j == 0) {
+					answer = j / 3;
+					break;
+				}
+			}
+		}
+		
+		System.out.println(answer);
+
 	}
 
 }
