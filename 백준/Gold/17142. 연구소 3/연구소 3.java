e179777442ad6baef675aef589f9495ed02befd5
조담현 <j38903639@gmail.com>
2023-10-10 00:54:53 +0900

    [Gold III] Title: 연구소 3, Time: 260 ms, Memory: 42980 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.java
+++ 백준/Gold/17142. 연구소 3/연구소 3.java
@@ -1,107 +1,114 @@
-import java.util.*;
-import java.io.*;
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-class Main {
-    static class Virus {
-        int x, y, time;
+/*
+ * 1. 바이러스의 위치인 모든 2 중에서 M개를 골라 조합으로 경우의 수를 구한다.
+ * 2. M개의 바이러스를 시작점으로 BFS를 시작하여(배열 복사) 걸리는 시간을 측정하고, 모든 빈칸을 채웠을 경우 최소 시간을 구한다.
+ */
+public class Main {
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+	static int N, M;
+	static int[][] map;
+	static boolean[] selected;
+	static int min = Integer.MAX_VALUE;
+	static Point[] startPoint;
+	static List<Point> virusList = new ArrayList<>();
 
-        Virus(int x, int y, int time) {
-            this.x = x;
-            this.y = y;
-            this.time = time;
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N][N];
+		startPoint = new Point[M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] == 2) {
+					virusList.add(new Point(i, j));
+				}
+			}
+		}
+		selected = new boolean[virusList.size()];
 
-    static int N, M;
-    static int[][] arr;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static List<Virus> viruses = new ArrayList<>();
-    static Virus[] active;
-    static int resultMinTime = Integer.MAX_VALUE;
-    static int originEmptySpace = 0;
+		backTracking(0, 0);
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+		if (min == Integer.MAX_VALUE) {
+			System.out.println(-1);
+		} else {
+			System.out.println(min);
+		}
+	}// main
 
-        // input
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        
-        arr = new int[N][N];
-        active = new Virus[M];
+	public static void backTracking(int start, int depth) {
+		if (depth == M) {
+			bfs();
+			return;
+		}
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+		for (int i = start; i < virusList.size(); i++) {
+			if (!selected[i]) {
+				selected[i] = true;
+				startPoint[depth] = virusList.get(i);
+				backTracking(i, depth + 1);
+				selected[i] = false;
+			}
+		}
+	}
 
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+	public static void bfs() {
+		Queue<Point> queue = new LinkedList<>();
+		boolean[][] visited = new boolean[N][N];
+		int[][] temp = new int[N][N];
+		for (Point p : startPoint) {
+			queue.add(p);
+			visited[p.x][p.y] = true;
+			temp[p.x][p.y] = 0;
+		}
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				Point now = queue.poll();
+				for (int d = 0; d < 4; d++) {
+					int nr = now.x + dr[d];
+					int nc = now.y + dc[d];
+					if (nr >= 0 && nc >= 0 && nr < N && nc < N && !visited[nr][nc] && map[nr][nc] != 1) {
+						temp[nr][nc] = temp[now.x][now.y] + 1;
+						if (temp[nr][nc] >= min && map[nr][nc] == 0)
+							return;
+						visited[nr][nc] = true;
+						queue.add(new Point(nr, nc));
+					}
+				}
+			}
 
-                if (arr[i][j] == 0) {
-                    originEmptySpace++;
-                } else if (arr[i][j] == 2) {
-                    viruses.add(new Virus(i, j, 0));
-                }
-            }
-        }
+		}
 
-        // solution
-        if (originEmptySpace == 0) {
-            System.out.println(0);
-        } else {
-            selectVirus(0, 0);
-            System.out.println(resultMinTime == Integer.MAX_VALUE ? -1 : resultMinTime);
-        }
-    }
+		checkVirus(temp);
 
-    // 백트래킹으로 M 개의 바이러스를 선택하는 Combination 구현
-    static void selectVirus(int start, int selectCount) {
-        if (selectCount == M) {
-            spreadVirus(originEmptySpace);
-            return;
-        }
+	}
 
-        for (int i = start; i < viruses.size(); i++) {
-            active[selectCount] = viruses.get(i);
-            selectVirus(i + 1, selectCount + 1);
-        }
-    }
+	public static void checkVirus(int [][] temp) {
+		int time = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (temp[i][j]==0 && map[i][j] == 0) {
+					return;
+				}
+				if (temp[i][j]!=0&& map[i][j]==0) {
+					time = Math.max(time, temp[i][j]);
+				}
+			}
+		}
+		min = Math.min(min, time);
+	}
 
-    // BFS 로 바이러스를 퍼트린다
-    static void spreadVirus(int emptySpace) {
-        Queue<Virus> q = new LinkedList<>();
-        boolean[][] infected = new boolean[N][N];
-
-        for (int i = 0; i < M; i++) {
-            Virus virus = active[i];
-            infected[virus.x][virus.y] = true;
-            q.add(virus);
-        }
-
-        while (!q.isEmpty()) {
-            Virus virus = q.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = virus.x + dx[i];
-                int ny = virus.y + dy[i];
-
-                if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-                if (infected[nx][ny] || arr[nx][ny] == 1) continue;
-
-                if (arr[nx][ny] == 0) {
-                    emptySpace--;
-                }
-
-                if (emptySpace == 0) {
-                    resultMinTime = Math.min(resultMinTime, virus.time + 1);
-                    return;
-                }
-
-                infected[nx][ny] = true;
-                q.add(new Virus(nx, ny, virus.time + 1));
-            }
-        }
-    }
 }
