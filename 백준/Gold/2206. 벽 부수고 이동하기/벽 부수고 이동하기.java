d0583a1c80d6d19ff843b5c068218c66e842e3ec
조담현 <j38903639@gmail.com>
2023-10-19 14:13:25 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 768 ms, Memory: 161572 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,88 +1,93 @@
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
-class Point {
-	int x;
-	int y;
-	boolean broken;
-	
-	Point(int x, int y, boolean broken) {
-		this.x=x;
-		this.y=y;
-		this.broken=broken;
-	}
-}
+
 public class Main {
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
+
+	static class human {
+		public human(int x, int y, int cnt, boolean boom) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.cnt = cnt;
+			this.boom = boom;
+		}
+
+		int x;
+		int y;
+		int cnt;
+		boolean boom;
+
+	}
+
 	static int N, M;
 	static int[][] map;
+	static boolean[][][] check;
+	static Queue<human> que = new LinkedList<>();
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+
 		map = new int[N][M];
 		for (int i = 0; i < N; i++) {
 			String str = br.readLine();
 			for (int j = 0; j < M; j++) {
-				map[i][j] = str.charAt(j) - '0';
+				map[i][j] = str.charAt(j)-'0';
 			}
 		}
 
-		System.out.println(bfs());
+		bfs(0, 0, 1, false);
 	}
 
-	public static int bfs() {
-		Queue<Point> queue = new LinkedList<>();
-		int[][][] dist = new int[N][M][2];
-		queue.add(new Point(0, 0, false));
-		dist[0][0][0] = 1;
-		while (!queue.isEmpty()) {
-			Point now = queue.poll();
-			if(now.x==N-1&&now.y==M-1) return dist[now.x][now.y][0];
+	private static void bfs(int i, int j, int k, boolean b) {
+
+		que.add(new human(i, j, k, b));
+
+		check = new boolean[N][M][2];
+		while (!que.isEmpty()) {
+			human now = que.poll();
+			if(now.x == N-1 && now.y == M-1) {
+				System.out.println(now.cnt);
+				return;
+			}
+
 			for (int d = 0; d < 4; d++) {
-				int nr = now.x + dr[d];
-				int nc = now.y + dc[d];
-				if (nr >= 0 && nc >= 0 && nr < N && nc < M) {
-					// 다음 칸이 벽이 아닐 때
-					if(map[nr][nc]==0) {
-						// 벽을 부수고 왔으면
-						if(now.broken&&dist[nr][nc][1]==0) {
-							dist[nr][nc][1] = dist[now.x][now.y][1] + 1;
-							queue.add(new Point(nr, nc, true));
-							if(nr==N-1&&nc==M-1) return dist[nr][nc][1];
-						}
-						// 벽을 안 부수고 왔으면
-						else if(!now.broken&&dist[nr][nc][0]==0) {
-							dist[nr][nc][0] = dist[now.x][now.y][0] + 1;
-							queue.add(new Point(nr, nc, false));
-							if(nr==N-1&&nc==M-1) return dist[nr][nc][0];
-						}
+				int nx = now.x + dr[d];
+				int ny = now.y + dc[d];
+
+				if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+					continue;
+				}
+
+				int nc = now.cnt + 1;
+
+				if (map[nx][ny] == 0) {
+					if (!now.boom && !check[nx][ny][0]) {
+						que.add(new human(nx, ny, nc, false));
+						check[nx][ny][0] = true;
+					} else if (now.boom && !check[nx][ny][1]) {
+						que.add(new human(nx, ny, nc, true));
+						check[nx][ny][1] = true;
 					}
-					
-					// 다음 칸이 벽일 때 
-					if(map[nr][nc]==1) {
-						// 벽을 부수고 왔으면
-						if(now.broken) {
-							continue;
-						}
-						// 벽을 안부수고 왔으면 벽을 부순다.
-						else if(!now.broken&&dist[nr][nc][1]==0) {
-							dist[nr][nc][1] = dist[now.x][now.y][0] + 1;
-							queue.add(new Point(nr, nc, true));
-							if(nr==N-1&&nc==M-1) return dist[nr][nc][1];
-						}
+				}else if(map[nx][ny] == 1) {
+					if(!now.boom) {
+						que.add(new human(nx,ny,nc,true));
+						check[nx][ny][1] = true;
 					}
-					
 				}
 			}
 		}
-		return -1;
+		System.out.println(-1);
 	}
-
 }
