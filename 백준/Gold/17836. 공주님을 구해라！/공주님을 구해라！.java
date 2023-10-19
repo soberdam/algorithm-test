0a27eff8c39043bf96342d093619feb565ce683d
조담현 <j38903639@gmail.com>
2023-10-19 15:44:49 +0900

    [Gold V] Title: 공주님을 구해라!, Time: 124 ms, Memory: 13404 KB -BaekjoonHub

--- 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.java
+++ 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.java
@@ -1,97 +1,83 @@
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static class Hero {
-		int x;
-		int y;
-		int cnt;
-		boolean gram;
 
-		public Hero(int x, int y, int cnt, boolean gram) {
-			this.x = x;
-			this.y = y;
-			this.cnt = cnt;
-			this.gram = gram;
-		}
-	}
+    static int N, M, T;
+    static int[][] map;
+    static int[][] countmap;
+    static boolean[][] visit;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
 
-	static int N, M, T;
-	static int[][] map;
-	static boolean[][][] check;
-	static int[] dr = { 1, -1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        countmap = new int[N][M];
+        visit = new boolean[N][M];
+        Point swd = new Point(0, 0);
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 2) {
+                    swd.x = i;
+                    swd.y = j;
+                }
+            }
+        }
+        int min =0;
+        int swdcount = 0;
+        go();
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+        if (!visit[swd.x][swd.y]) {
+            if (visit[N-1][M-1] && countmap[N-1][M-1] <= T) {
+                System.out.println(countmap[N-1][M-1]);
+            } else {
+                System.out.println("Fail");
+            }
+        } else {
+            swdcount = countmap[swd.x][swd.y] + (N - 1 - swd.x) + (M - 1 - swd.y);
 
-		st = new StringTokenizer(br.readLine());
+            if(visit[N-1][M-1]){
+                min = Math.min(swdcount,countmap[N-1][M-1]);
+                if(min<=T) System.out.println(min);
+                else System.out.println("Fail");
+            }else if(!visit[N-1][M-1]){
+                if(swdcount<=T) System.out.println(swdcount);
+                else System.out.println("Fail");
+            }
+        }
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		T = Integer.parseInt(st.nextToken());
+    }
 
-		map = new int[N][M];
-
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-
-		check = new boolean[N][M][2];
-		bfs(0, 0);
-
-	}
-
-	public static void bfs(int x, int y) {
-		Queue<Hero> q = new LinkedList<>();
-		q.add(new Hero(x, y, 0, false));
-		check[x][y][0] = true;
-
-		while (!q.isEmpty()) {
-			Hero now = q.poll();
-
-			if (now.cnt > T)
-				break;
-			if (now.x == N - 1 && now.y == M - 1) {
-				System.out.println(now.cnt);
-				return;
-			}
-
-			for (int d = 0; d < 4; d++) {
-				int nx = now.x + dr[d];
-				int ny = now.y + dc[d];
-
-				if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
-					continue;
-				}
-
-				if (!now.gram) {
-					if (!check[nx][ny][0] && map[nx][ny] == 0) {
-						q.add(new Hero(nx, ny, now.cnt + 1, now.gram));
-						check[nx][ny][0] = true;
-					} else if (!check[nx][ny][0] && map[nx][ny] == 2) {
-						q.add(new Hero(nx, ny, now.cnt + 1, true));
-						check[nx][ny][0] = true;
-					}
-
-				} else {
-					if (!check[nx][ny][1]) {
-						q.add(new Hero(nx, ny, now.cnt + 1, now.gram));
-						check[nx][ny][1] = true;
-
-					}
-				}
-			}
-		}
-		System.out.println("Fail");
-
-	}
-
+    private static void go() {
+        Queue<Point> qu = new LinkedList<>();
+        qu.add(new Point(0, 0));
+        visit[0][0] = true;
+        while (!qu.isEmpty()) {
+            Point now = qu.poll();
+            int x = now.x;
+            int y = now.y;
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dr[i];
+                int ny = y + dc[i];
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M || visit[nx][ny] || map[nx][ny] == 1)
+                    continue;
+                visit[nx][ny] = true;
+                countmap[nx][ny] = countmap[x][y] + 1;
+                qu.add(new Point(nx, ny));
+            }
+        }
+    }
 }
