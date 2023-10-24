2e4287aab554e393ccdbb91b7d47160a5ca83280
조담현 <j38903639@gmail.com>
2023-10-24 10:16:02 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 732 ms, Memory: 69160 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,117 +1,101 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
- 
-class Node implements Comparable<Node> {
-    int end;
-    int weight;
- 
-    Node(int end, int weight) {
-        this.end = end;
-        this.weight = weight;
-    }
- 
-    @Override
-    public int compareTo(Node o) {
-        return weight - o.weight;
-    }
- 
+
+class Node implements Comparable {
+	int index;
+	int cost;
+	
+	Node(int index, int cost) {
+		this.index = index;
+		this.cost = cost;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		return cost - ((Node)o).cost;
+	}
+	
 }
- 
 public class Main {
-    static int N, E;
-    static ArrayList<ArrayList<Node>> a; // 인접리스트.
-    static int[] dist; // 시작점에서 각 정점으로 가는 최단거리.
-    static boolean[] check; // 방문 확인.
-    static final int INF = 200000000;
- 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
- 
-        a = new ArrayList<>();
-        dist = new int[N + 1];
-        check = new boolean[N + 1];
- 
-        Arrays.fill(dist, INF);
- 
-        for (int i = 0; i <= N; i++) {
-            a.add(new ArrayList<>());
-        }
- 
-        // 양방향 인접 리스트 구현.
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
- 
-            // start에서 end로 가는 weight (가중치)
-            a.get(start).add(new Node(end, weight));
- 
-            // end에서 start로 가는 weight (가중치)
-            a.get(end).add(new Node(start, weight));
-        }
- 
-        // 반드시 거쳐야 하는 정점.
-        st = new StringTokenizer(br.readLine());
-        int v1 = Integer.parseInt(st.nextToken());
-        int v2 = Integer.parseInt(st.nextToken());
- 
-        // 1 -> v1 -> v2 -> N으로 가는 경우
-        int res1 = 0;
-        res1 += dijkstra(1, v1);
-        res1 += dijkstra(v1, v2);
-        res1 += dijkstra(v2, N);
- 
-        // 1 -> v2 -> v1 -> N으로 가는 경우
-        int res2 = 0;
-        res2 += dijkstra(1, v2);
-        res2 += dijkstra(v2, v1);
-        res2 += dijkstra(v1, N);
- 
-        int ans = (res1 >= INF && res2 >= INF) ? -1 : Math.min(res1, res2);
- 
-        bw.write(ans + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
- 
-    // 다익스트라 알고리즘
-    public static int dijkstra(int start, int end) {
-        Arrays.fill(dist, INF);
-        Arrays.fill(check, false);
- 
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        boolean[] check = new boolean[N + 1];
-        pq.offer(new Node(start, 0));
-        dist[start] = 0;
- 
-        while (!pq.isEmpty()) {
-            Node curNode = pq.poll();
-            int cur = curNode.end;
- 
-            if (!check[cur]) {
-                check[cur] = true;
- 
-                for (Node node : a.get(cur)) {
-                    if (!check[node.end] && dist[node.end] > dist[cur] + node.weight) {
-                        dist[node.end] = dist[cur] + node.weight;
-                        pq.add(new Node(node.end, dist[node.end]));
-                    }
-                }
-            }
-        }
- 
-        return dist[end];
-    }
+	static int N, E;
+	static boolean[] visited;
+	static int[] dist;
+	static int answer;
+	static List<List<Node>> graph = new ArrayList<>();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		
+		
+		answer = 0;
+		
+		for(int i=0; i<N+1; i++) {
+			graph.add(new ArrayList<Node>());
+		}
+		for(int i=0; i<E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			graph.get(a).add(new Node(b, c));
+			graph.get(b).add(new Node(a, c));
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		int v1 = Integer.parseInt(st.nextToken());
+		int v2 = Integer.parseInt(st.nextToken());
+		
+		int res1 = 0;
+		res1 += dijkstra(1, v1);
+		res1 += dijkstra(v1, v2);
+		res1 += dijkstra(v2, N);
+		
+		int res2 = 0;
+		res2 += dijkstra(1, v2);
+		res2 += dijkstra(v2, v1);
+		res2 += dijkstra(v1, N);
+		
+		if(res1>=10000000&&res2>=10000000) {
+			answer = -1;
+		} else {
+			answer = Math.min(res1, res2);
+		}
+		
+		System.out.println(answer);
+	}
+	
+	public static int dijkstra(int start, int end) {
+		visited = new boolean[N+1];
+		dist = new int[N+1];
+		Arrays.fill(dist, 10000000);
+		
+		PriorityQueue<Node> pq = new PriorityQueue<>(); 
+		dist[start] = 0;
+		pq.add(new Node(start, 0));
+		
+		while(!pq.isEmpty()) {
+			Node cur = pq.poll();
+			
+			if(visited[cur.index]) continue;
+			visited[cur.index] = true;
+			
+			for(Node next : graph.get(cur.index)) {
+				if(!visited[next.index]&&dist[cur.index]+next.cost<dist[next.index]) {
+					dist[next.index] = dist[cur.index]+next.cost;
+					pq.add(new Node(next.index, dist[next.index]));
+				}
+			}
+		}
+		
+		return dist[end];
+	}
 }
