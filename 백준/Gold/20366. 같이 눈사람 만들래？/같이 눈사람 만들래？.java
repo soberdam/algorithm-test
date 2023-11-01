a00c7480405f90be1092965b27d8f24c5096e4de
조담현 <j38903639@gmail.com>
2023-11-01 23:47:34 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 240 ms, Memory: 14516 KB -BaekjoonHub

---
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int arr[] = new int[N];
+		for (int i = 0; i < N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(arr);
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				int snowMan1 = arr[i] + arr[j];
+				int start = 0;
+				int end = N - 1;
+				while (start < end) {
+					if (start == i || start == j) {
+						start++;
+						continue;
+					}
+					if (end == i || end == j) {
+						end--;
+						continue;
+					}
+					int snowMan2 = arr[start] + arr[end];
+					min = Math.min(min, Math.abs(snowMan1 - snowMan2));
+					if (snowMan1 > snowMan2)
+						start++;
+					else if (snowMan1 < snowMan2)
+						end--;
+					else {
+						System.out.println(0);
+						return;
+					}
+				}
+			}
+		}
+		System.out.println(min);
+	}
+}
