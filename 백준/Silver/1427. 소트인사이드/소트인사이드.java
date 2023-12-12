21405f6089ef9ae375a0c1a9574393991958fe15
조담현 <j38903639@gmail.com>
2023-12-12 23:56:45 +0900

    [Silver V] Title: 소트인사이드, Time: 132 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int[] counting = new int[10];
+		String s = br.readLine();
+ 
+		for (int i = 0; i < s.length(); i++) {
+			counting[s.charAt(i) - '0']++;
+		}
+ 
+		for (int i = 9; i >= 0; i--) {
+			while (counting[i]-- > 0) {
+				System.out.print(i);
+			}
+		}
+	}
+}
