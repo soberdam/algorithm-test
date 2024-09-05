9653ffabf7368bf1bd9879d013c939b23b47c71e
조담현 <j38903639@gmail.com>
2024-09-05 15:31:17 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.12 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<k; i++) {
+            sb.append(my_string);
+        }
+        
+        answer = sb.toString();
+        return answer;
+    }
+}
