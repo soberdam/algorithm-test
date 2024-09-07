0412f760656ee4ed9e80125c643f58fb5f459605
조담현 <j38903639@gmail.com>
2024-09-07 10:41:06 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int [num_list.length+1];
+        System.arraycopy(num_list,0,answer,0,num_list.length);
+        if(num_list[num_list.length-1]>num_list[num_list.length-2]) {
+            answer[answer.length-1] = num_list[num_list.length-1] - num_list[num_list.length-2];
+        } else {
+            answer[answer.length-1] = num_list[num_list.length-1] *2;
+        }
+        return answer;
+    }
+}
